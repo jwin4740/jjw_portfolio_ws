4be75b0fe6159ad010a0bde5e70b7f685a8d0fdd
jwin4740 <jwin4740@gmail.com>
2017-10-31 02:42:54 -0400

    skeleton

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http = require('http');
+
 
 const port = 8080;
 
-var app = express();
+const app = express();
 
 
 app.use(bodyParser.json());
@@ -22,12 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.send('index.html');
 });
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
 
 
 app.listen(port, function () {
