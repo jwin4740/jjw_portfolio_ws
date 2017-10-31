8605c7f1d8863035c10cec4952b47a40eed69e54
jwin4740 <jwin4740@gmail.com>
2017-10-31 02:08:07 -0400

    myport

---
+++ app.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env nodejs
+
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+const port = 8080;
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    res.send('index.html');
+});
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+
+app.listen(port, function () {
+    console.log('server listening on port:', port)
+});
+
